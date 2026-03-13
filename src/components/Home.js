3dc5afce824005a0b34d36330810dbab30dc9631
Smoke5643 <112447379+Smoke5643@users.noreply.github.com>
2026-03-12 23:11:21 -0400

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -155,28 +155,12 @@ const Home = () => {
           <p>Track your shiny National Dex completion.</p>
         </Card>
         <Card to="/shiny-tracker/gender">
-          <h2>Gender Dex</h2>
-          <p>Track your gender variants.</p>
-        </Card>
-        <Card to="/shiny-tracker/dex">
-          <h2>Dex</h2>
-          <p>Track your National Dex completion.</p>
-        </Card>
-        <Card to="/shiny-tracker/godex">
-          <h2>Go Dex</h2>
-          <p>Track your Pokémon Go Dex completion.</p>
-        </Card>
-        <Card to="/shiny-tracker/unown">
-          <h2>Unown Dex</h2>
-          <p>Track how many Unown you own.</p>
+          <h2>Shiny Gender Dex</h2>
+          <p>Track your shiny gender variants.</p>
         </Card>
         <Card to="/shiny-tracker/shinyunown">
           <h2>Shiny Unown Dex</h2>
           <p>Track how many shiny Unown you own.</p>
-        </Card>
-        <Card to="/shiny-tracker/shadow">
-          <h2>Shadow Dex</h2>
-          <p>Track how many shadow Pokémon you have.</p>
         </Card>
       </CardContainer>
       </Container>
